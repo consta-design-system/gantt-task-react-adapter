723b7db7ff69da4521955df5c5602fb004a71479
arhayka <arhayka@gmail.com>
2022-09-02 11:45:40 +0300

    docs(common): fix image & texts (#13)

--- src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
+++ src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
@@ -1,10 +1,13 @@
 import { createStand } from '##/stand/standConfig';
 
+import image from './useGanttTaskReactAdapter.image.svg';
+
 export default createStand({
   title: 'useGanttTaskReactAdapter',
   id: 'useGanttTaskReactAdapter',
   group: 'components',
-  description: 'Этот адаптер стилизует диаграмму Ганта.',
+  image,
+  description: 'Адаптер для диаграммы Ганта.',
   version: '0.1.3',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D445%253A29188',
