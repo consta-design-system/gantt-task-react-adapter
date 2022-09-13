7b0657ff98023319a76cf4f5d594a629a47a8198
N1MBER <socialfox33@gmail.com>
2022-09-13 18:43:15 +0300

    docs(sandbox): add sandboxes links
    consta-design-system/uikit/#2615

--- src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
+++ src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
@@ -6,6 +6,8 @@ export default createStand({
   group: 'components',
   description: 'Этот адаптер стилизует диаграмму Ганта.',
   version: '0.1.3',
+  sandbox:
+    'https://codesandbox.io/embed/usegantttaskreactadapter-jcszt7?fontsize=14&hidenavigation=1&theme=dark',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D445%253A29188',
   status: 'stable',
