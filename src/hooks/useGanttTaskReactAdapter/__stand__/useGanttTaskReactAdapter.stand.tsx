c63d354f166098865e8ffc7d1499116217d07e81
N1MBER <socialfox33@gmail.com>
2022-09-14 17:22:13 +0300

    fix(sandbox): rewrite links to ids

--- src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
+++ src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
@@ -6,8 +6,7 @@ export default createStand({
   group: 'components',
   description: 'Этот адаптер стилизует диаграмму Ганта.',
   version: '0.1.3',
-  sandbox:
-    'https://codesandbox.io/embed/usegantttaskreactadapter-jcszt7?fontsize=14&hidenavigation=1&theme=dark',
+  sandbox: 'usegantttaskreactadapter-jcszt7',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D445%253A29188',
   status: 'stable',
