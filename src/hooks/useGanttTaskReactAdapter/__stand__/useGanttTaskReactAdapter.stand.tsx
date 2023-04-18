ef2f5d8291fa9a7cf4da1f960511b3e3a2986f21
Maxim Kolesnikov <socialfox33@gmail.com>
2023-04-18 13:53:41 +0300

    docs(useGanttTaskReactAdapter): add aliases

--- src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
+++ src/hooks/useGanttTaskReactAdapter/__stand__/useGanttTaskReactAdapter.stand.tsx
@@ -7,6 +7,16 @@ export default createStand({
   description: 'Адаптер для диаграммы Ганта.',
   version: '0.1.3',
   sandbox: 'usegantttaskreactadapter-jcszt7',
+  alias: [
+    'адаптер',
+    'диаграмма',
+    'гант',
+    'календарь',
+    'события',
+    'событие',
+    'задачи',
+    'план',
+  ],
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D445%253A29188',
   status: 'stable',
