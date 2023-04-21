5f1e8e71560ad46abecb9265ba8d2926d659550a
Maxim Kolesnikov <socialfox33@gmail.com>
2023-04-21 12:06:03 +0300

    fix(useGanttTaskReactAdapter): fix styles

--- src/hooks/useGanttTaskReactAdapter/__stand__/examples/GanttTaskReactAdapterExample/GanttTaskReactAdapterExample.tsx
+++ src/hooks/useGanttTaskReactAdapter/__stand__/examples/GanttTaskReactAdapterExample/GanttTaskReactAdapterExample.tsx
@@ -5,7 +5,11 @@ import { data } from '##/hooks/useGanttTaskReactAdapter/__mock__/mock.data';
 import { useGanttTaskReactAdapter } from '##/hooks/useGanttTaskReactAdapter/useGanttTaskReactAdapter';
 
 export const GanttTaskReactAdapterExample = () => {
-  const styleOptions = useGanttTaskReactAdapter({});
+  const { prefix, ...styleOptions } = useGanttTaskReactAdapter({});
 
-  return <Gantt tasks={data} {...styleOptions} />;
+  return (
+    <div className={prefix}>
+      <Gantt tasks={data} {...styleOptions} />
+    </div>
+  );
 };
