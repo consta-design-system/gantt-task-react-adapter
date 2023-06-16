bdc3b220d305f10da4cb4ab13d1457709cfa8a40
gizeasy <gizeasy@gmail.com>
2023-06-16 15:29:49 +0300

    dd

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -25,5 +25,5 @@ export const { createStand } = createConfig({
   repositoryUrl:
     'https://github.com/consta-design-system/gantt-task-react-adapter',
   figmaUrl:
-    'https://www.figma.com/file/LKYyjgQ3khFHvXraZAG1n1/Consta-Components?type=design&node-id=445-29188&t=DKeTFCEKhSmLPtik-0',
+    'https://www.figma.com/file/3RsiLTgTuXpdnqG7gW8UwL/Consta-Components-(Community)?type=design&node-id=445-29188&t=ponDmJar7RUOypIn-0',
 });
